ee79517010f59feec567ab8b37a09f9f6455be95
VadymRyzhuk <ceasarj571@gmail.com>
2023-12-11 19:34:06 +0100

    task 1-2

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import css from  "./Statistics.module.css"
+const Statistics = ({stats, title}) => {
+  return (
+    <>
+      <section className={css.statistics}>
+      {title.length > 0 && <h2 className={css.title}>Upload stats</h2>}
+  
+      <ul className={css.statList}>
+ 
+        {stats.map(item => {
+          return <li className={css.item} key={item.id}>
+            
+            <span className={css.label}>{item.label}</span>
+            <span className={css.percentage}>{item.percentage}%</span>
+    </li>
+            })}
+        
+  </ul>
+    </section>
+    </>
+  )
+}
+
+export { Statistics };
