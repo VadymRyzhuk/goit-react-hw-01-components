ec87c376295620435ce8377a6329e5b7c9b0627d
VadymRyzhuk <ceasarj571@gmail.com>
2023-12-12 17:17:07 +0100

    finish vers. 1.0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import { Profile } from "./Profile/Profile";
 import { Statistics } from "./Statistics/Statistics";
 import { FriendList } from "./FriendList/FriendList";
-//import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 import user from './Profile/user.json'
 import data from './Statistics/data.json'
 import friends from './FriendList/friends.json'
+import items from './TransactionHistory/transactions.json'
 
 
 export const App = () => {
@@ -18,9 +19,12 @@ export const App = () => {
       
       <Statistics title="Upload stats" stats={data} />
 
-<FriendList friends={friends}/>
+      <FriendList friends={friends} />
+      
+      <TransactionHistory items={items} />
     </>
 
+   
     
   );
 };
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import css from './TransactionHistory.module.css'
 
-const TransactionHistory = () => {
+const TransactionHistory = ({items}) => {
   return (
-    <div>TransactionHistory</div>
+    
+    <>
+    
+      <table className={css.transactionHistory}>
+  <thead>
+    <tr>
+      <th>Type</th>
+      <th>Amount</th>
+      <th>Currency</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    {items.map(item => (
+      <tr key={item.id}>
+        <td>{item.type}</td>
+        <td>{item.amount}</td>
+        <td>{item.currency}</td>
+      </tr>
+    ))}
+  </tbody>
+</table>
+
+      
+    </>
   )
 }
 
